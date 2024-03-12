b1f6ba7b3e2569ab2c99f33cf5882a703ad559b1
Jaehyun Ahn <desktop3711@gmail.com>
2024-03-12 17:14:50 +0900

    docs: 블로그 정보 업데이트

--- site.config.js
+++ site.config.js
@@ -1,31 +1,35 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "유우비트",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Backend Developer",
+    bio: "글 쓰는 개발자 유우비트입니다.",
+    email: "uwoobeat@gmail.com",
+    linkedin: "uwoobeat",
+    github: "uwoobeat",
+    instagram: "uwoobeat",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `10MM - 하루 10분으로 시작하는 습관 만들기`,
+      href: "https://github.com/depromeet/10mm-server"
     },
+    {
+      name: 'GDSC Hongik 플랫폼',
+      href: "https://github.com/GDSC-Hongik/gdsc-server"
+    }
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "글 쓰는 코딩노예",
+    description: "
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: "https://uwoobeat.dev",
+  since: 2023, // If leave this empty, current year will be used.
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
@@ -68,7 +72,7 @@ const CONFIG = {
     },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-  revalidateTime: 21600 * 7, // revalidate time for [slug], index
+  revalidateTime: 60, // revalidate time for [slug], index
 }
 
 module.exports = { CONFIG }
