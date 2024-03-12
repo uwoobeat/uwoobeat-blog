9fdb363c741f798d4f851c4c1cc0cc2cd71ef1e0
Jaehyun Ahn <desktop3711@gmail.com>
2024-03-12 17:15:39 +0900

    chore: 어터런스 비활성화

--- site.config.js
+++ site.config.js
@@ -57,7 +57,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
